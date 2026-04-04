eb9aa3396daa5119136f26d037d5487ecd96c857
JORDAN SOPLIN <jordansoplin99@gmail.com>
2026-04-04 10:06:02 -0500

    Cree las caracteristicas del heroe y su historia

---
+++ nivel_1_variables/heroe1.js
@@ -0,0 +1,9 @@
+// 1. Variables de héroe
+let nombreHeroe = "Thor";
+let poder = "Trueno";
+let niveldeEnergia = 85;
+let ciudad = "Asgard";
+let enemigo = "Loki";
+
+// 2. Historia usando console
+console.log("El nombre del héroe es: " + nombreHeroe + " con el poder del: " + poder + " con una energia de: " + niveldeEnergia + " en la ciudad de: " + ciudad + " su enemigo es: " + enemigo);
