dfedd29ee9f983995458d3afa1b73f2d356bbed2
JORDAN SOPLIN <jordansoplin99@gmail.com>
2026-04-04 10:11:36 -0500

    Se cambio el nivel de energia, su ciudad y su enemigo

--- nivel_1_variables/heroe2.js
+++ nivel_1_variables/heroe2.js
@@ -1,7 +1,7 @@
 let nombreHeroe = "Stark";
 let poder = "super inteligencia, velocidad y alta resistencia";
-let nivelEnergia = 170;
-let ciudad = "América";
-let enemigo = "Ryu";
+let nivelEnergia = 70;
+let ciudad = "New York";
+let enemigo = "Venom";
 
 console.log(nombreHeroe + " un héroe con los poderes de " + poder + ", regresa al lugar donde nació que es " + ciudad + ", donde combate contra " + enemigo + " con su energía al " + nivelEnergia + " por ciento.")
