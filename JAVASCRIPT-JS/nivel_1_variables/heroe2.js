629f2d579dca0afbcd033cc32296f92c0781eff3
JORDAN SOPLIN <jordansoplin99@gmail.com>
2026-04-06 13:43:59 -0500

    Se mejoro la forma de leer, enves de leer una sola linea, se uso un salto de linea para leer intercaladamente

--- JAVASCRIPT-JS/nivel_1_variables/heroe2.js
+++ JAVASCRIPT-JS/nivel_1_variables/heroe2.js
@@ -4,4 +4,4 @@ let nivelEnergia = 70;
 let ciudad = "New York";
 let enemigo = "Venom";
 
-console.log("El nombre del heroe es: " + nombreHeroe + ", un héroe con los poderes de " + poder + ", regresa al lugar donde nació que es " + ciudad + ", donde combate contra su mayor enemigo " + enemigo + ", y su nivel de energía es de: " + nivelEnergia + " por ciento.")
+console.log("El nombre del heroe es: " + nombreHeroe + "\n Un héroe con los poderes de: " + poder + " \n Regresa al lugar donde nació que es: " + ciudad + "\n Donde suele combatir contra su mayor enemigo: " + enemigo + "\n Su nivel de energía es de: " + nivelEnergia + " por ciento.")
