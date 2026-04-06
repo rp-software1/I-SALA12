6b56b281842f6724b5d4942656db1c4a97135802
Yojan1409 <1561124@senati.pe>
2026-04-06 09:31:31 -0500

    Se agregó la función de pagar cliente1

--- nivel_3_objetos/simulador1.js
+++ nivel_3_objetos/simulador1.js
@@ -7,5 +7,12 @@ let cliente = {
 function ordenarComida(cliente) {
     console.log(cliente.nombre + " ordenó comida en Burger King.");
 }
+function pagar(cliente) {
+    cliente.dinero -= 10
+    cliente.hambre -= 60
+    console.log(cliente.nombre + " pagó su comida y ahora tiene " + cliente.dinero + " soles.");
+}
+
 
 ordenarComida(cliente);
+pagar(cliente);
