ffa05b8d631b0308ca910f1570ae983d071026eb
JORDAN SOPLIN <jordansoplin99@gmail.com>
2026-04-06 13:37:06 -0500

    Se mejoro la interacción del heroe2, mejorando asi su breve historia y puntuaciones

--- JAVASCRIPT-JS/nivel_1_variables/heroe2.js
+++ JAVASCRIPT-JS/nivel_1_variables/heroe2.js
@@ -4,4 +4,4 @@ let nivelEnergia = 70;
 let ciudad = "New York";
 let enemigo = "Venom";
 
-console.log(nombreHeroe + " un héroe con los poderes de " + poder + ", regresa al lugar donde nació que es " + ciudad + ", donde combate contra " + enemigo + " con su energía al " + nivelEnergia + " por ciento.")
+console.log("El nombre del heroe es: " + nombreHeroe + ", un héroe con los poderes de " + poder + ", regresa al lugar donde nació que es " + ciudad + ", donde combate contra su mayor enemigo " + enemigo + ", y su nivel de energía es de: " + nivelEnergia + " por ciento.")
