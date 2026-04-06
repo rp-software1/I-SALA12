8505d1daa6349a6d7794b819c54b2d9bb35461f7
Yojan1409 <1561124@senati.pe>
2026-04-06 10:13:41 -0500

    Se agregó la función de entrenar para el RPG1 del jugador1

--- nivel_3_objetos/RPG1.js
+++ nivel_3_objetos/RPG1.js
@@ -3,4 +3,15 @@ let jugador1 = {
     vida: 100,
     fuerza: 120,
     nivel: 70,
+    pociones: 10
 }
+
+
+function entrenar(jugador1) {
+    jugador1.pociones += 5
+    jugador1.fuerza += 20
+    jugador1.vida -= 10
+    console.log(jugador1.nombre + " entrena duramente contra un dragón, al vencerlo este les dropea pociones de vida y su fuerza incrementa a " + jugador1.fuerza + ".");
+}
+
+entrenar(jugador1);
