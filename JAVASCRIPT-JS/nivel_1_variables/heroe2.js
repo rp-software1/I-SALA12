5cbd8fa73e273c570329a353fb367891cbbba84d
JORDAN SOPLIN <jordansoplin99@gmail.com>
2026-04-06 13:47:32 -0500

    Se agrego un par de saltos de linea para el heroe2

--- JAVASCRIPT-JS/nivel_1_variables/heroe2.js
+++ JAVASCRIPT-JS/nivel_1_variables/heroe2.js
@@ -4,4 +4,4 @@ let nivelEnergia = 70;
 let ciudad = "New York";
 let enemigo = "Venom";
 
-console.log("El nombre del heroe es: " + nombreHeroe + "\n Un héroe con los poderes de: " + poder + " \n Regresa al lugar donde nació que es: " + ciudad + "\n Donde suele combatir contra su mayor enemigo: " + enemigo + "\n Su nivel de energía es de: " + nivelEnergia + " por ciento.")
+console.log("\n El nombre del heroe es: " + nombreHeroe + "\n Un héroe con los poderes de: " + poder + " \n Regresa al lugar donde nació que es: " + ciudad + "\n Donde suele combatir contra su mayor enemigo: " + enemigo + "\n Su nivel de energía es de: " + nivelEnergia + " por ciento.\n")
