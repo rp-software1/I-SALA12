a57810719a1aaec2dbc09c744c56cf1a5ec64863
Yojan1409 <1561124@senati.pe>
2026-04-06 09:33:16 -0500

    Se agregó la función de mostrarcliente1 para ver las actulizaciones

--- nivel_3_objetos/simulador1.js
+++ nivel_3_objetos/simulador1.js
@@ -7,12 +7,17 @@ let cliente = {
 function ordenarComida(cliente) {
     console.log(cliente.nombre + " ordenó comida en Burger King.");
 }
+
 function pagar(cliente) {
     cliente.dinero -= 10
     cliente.hambre -= 60
     console.log(cliente.nombre + " pagó su comida y ahora tiene " + cliente.dinero + " soles.");
 }
 
+function mostrar(cliente) {
+    console.log("Estado de " + cliente.nombre + ": dinero = " + cliente.dinero + ", hambre = " + cliente.hambre);
+}
 
 ordenarComida(cliente);
 pagar(cliente);
+mostrar(cliente);
