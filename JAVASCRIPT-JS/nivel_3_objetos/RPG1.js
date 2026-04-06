515b8e140aa064552c3d264b301809f85a8a549a
Yojan1409 <1561124@senati.pe>
2026-04-06 10:54:22 -0500

    Se agregó la función de curarse para el jugador1

--- JAVASCRIPT-JS/nivel_3_objetos/RPG1.js
+++ JAVASCRIPT-JS/nivel_3_objetos/RPG1.js
@@ -9,9 +9,18 @@ let jugador1 = {
 
 function entrenar(jugador1) {
     jugador1.pociones += 5
-    jugador1.fuerza += 20
+    jugador1.nivel += 20
     jugador1.vida -= 10
+
     console.log(jugador1.nombre + " entrena duramente contra un dragón, al vencerlo este les dropea pociones de vida y su fuerza incrementa a " + jugador1.fuerza + ".");
 }
 
+function curarse(jugador1) {
+    jugador1.vida += 14;
+    jugador1.pociones -= 1;
+
+    console.log("Posteriormente " + jugador1.nombre + " se cura con las pociones dropeadas; por lo tanto ahora tiene " + jugador1.vida + " de vida");
+}
+
 entrenar(jugador1);
+curarse(jugador1)
