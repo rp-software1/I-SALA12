6fdb211b7615240758681b510e92ae2b5ebd9bf4
Yojan1409 <1561124@senati.pe>
2026-04-06 09:29:39 -0500

    Se agregaron el objeto de cliente y también la función de ordenar

---
+++ nivel_3_objetos/simulador1.js
@@ -0,0 +1,11 @@
+let cliente = {
+    nombre: "Yojan",
+    dinero: 150,
+    hambre: 90
+};
+
+function ordenarComida(cliente) {
+    console.log(cliente.nombre + " ordenó comida en Burger King.");
+}
+
+ordenarComida(cliente);
