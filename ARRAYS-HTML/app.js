145632a5bd5915930ea12edd4668cda432f6c171
JORDAN SOPLIN <jordansoplin99@gmail.com>
2026-04-07 13:59:48 -0500

    Se agrego el nombre del restaurante y platos adicionales al menu en array

--- ARRAYS-HTML/app.js
+++ ARRAYS-HTML/app.js
@@ -2,7 +2,9 @@
 let menu = [
     { nombre: "Arroz con pollo", precio: 12, stock: 5 },
     { nombre: "Lomo saltado", precio: 18, stock: 3 },
-    { nombre: "Sopa", precio: 8, stock: 10 }
+    { nombre: "Sopa", precio: 8, stock: 10 },
+    { nombre: "Milanesa", precio: 12, stock: 4 },
+    { nombre: "Ceviche", precion: 25, stock: 8 }
 ];
 
 // 2) FUNCIÓN: renderizar (mostrar) el menú en pantalla
