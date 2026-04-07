091c99a6b241ea4649106239ffbd57bae6790276
Yojan1409 <1561124@senati.pe>
2026-04-07 10:30:54 -0500

    Se mejoró el sistema de orden de comida añadiendo otro objeto para el simulador2 y asu vez un mensaje de si el cliente quedó satisfecho

--- JAVASCRIPT-JS/nivel_3_objetos/simulador2.js
+++ JAVASCRIPT-JS/nivel_3_objetos/simulador2.js
@@ -4,20 +4,32 @@ let cliente = {
     hambre: 90
 };
 
-function ordenarComida(cliente) {
-    console.log(cliente.nombre + " Ordeno costilla de res a la BBQ");
+let comida = {
+    nombre: "costilla de res a la BBQ",
+    precio: 20
 }
 
-function pagar(cliente) {
-    cliente.dinero -= 70
+function ordenarComida(cliente, comida) {
+    console.log(cliente.nombre + " ordenó " + comida.nombre);
+}
+
+function pagar(cliente, comida) {
+    cliente.dinero = cliente.dinero - comida.precio
     cliente.hambre -= 40
     console.log(cliente.nombre + " cancelo su comida y ahora tiene " + cliente.dinero + " soles.");
 }
-
-function mostrar(cliente) {
-    console.log("Estado de " + cliente.nombre + ": dinero = " + cliente.dinero + ", hambre = " + cliente.hambre);
+function boleta(cliente, comida) {
+    console.log("La boleta que se le imprime al cliente sería tal que así:")
+    console.log("Nombre cliente: " + cliente.nombre)
+    console.log("Comida: " + comida.nombre)
+    console.log("Precio: " + comida.precio)
+}
+function estado(cliente) {
+    console.log("Y el estado de " + cliente.nombre + ": dinero = " + cliente.dinero + ", hambre = " + cliente.hambre);
+    console.log("Quedando así totalmente satisfecho con la atención brindada")
 }
 
-ordenarComida(cliente);
-pagar(cliente);
-mostrar(cliente);
+ordenarComida(cliente, comida);
+pagar(cliente, comida);
+boleta(cliente, comida);
+estado(cliente);
