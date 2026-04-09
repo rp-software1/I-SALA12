a277c6f0f18cd5f0a9c2390d7596c7fac88b72e0
JORDAN SOPLIN <jordansoplin99@gmail.com>
2026-04-09 12:48:07 -0500

    Se redujo un poco el nombre del boton stock

--- ARRAYS-HTML/app.js
+++ ARRAYS-HTML/app.js
@@ -38,8 +38,6 @@ function contarPlatos() {
     return menu.length;
 }
 
-
-
 // 4) EVENTOS: conectar botones con funciones
 document.getElementById("btnMostrar").addEventListener("click", () => {
     renderMenu();
