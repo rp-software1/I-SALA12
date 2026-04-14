0f9bcae080ab9b520f857b9b074bc27ce57c7a80
Yojan1409 <1561124@senati.pe>
2026-04-14 09:28:12 -0500

    Se agregó el main.js para conectar todo a futuro

---
+++ ARRAYS-HTML/main.js
@@ -0,0 +1,8 @@
+// main.js
+// Punto de entrada del Sistema
+// (Imports se completarán luego)
+// import { conectarEventos } from "./ui.js";
+
+export function iniciarApp() {
+    console.log("Sistema iniciado ( pendiente conexion completa)");
+}
